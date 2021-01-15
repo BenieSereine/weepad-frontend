3228f1fc520cee8a467aae3ce83a150d46523437
BenieSereine <BMugwaneza720@gmail.com>
2021-01-15 03:09:22 +0200

    Explore your favorite books

--- src/components/pages/write.js
+++ src/components/pages/write.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import '../../App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
-export default function Products() {
+import React, { useState } from "react";
+import "../Button.css"
+import { useSpring, animated } from "react-spring";
+
+function Create() {
+  const [FormStatus, setFormStatus] = useState(false);
+  const createProps = useSpring({ 
+    left: FormStatus ? -500 : 0,
+  });
+
+  const createBtnProps = useSpring({
+    borderBottom: FormStatus 
+      ? "solid 0px transparent"
+      : "solid 2px #1059FF", 
+  });
+  function createClicked() {
+    setFormStatus(false);
+  }
+
   return (
-    <div>
-    <h1 className='write'>Create your own story?<br></br>
-    Thank you for your interest in writing, you are on the right application. 
-    However, you first have to login to access your account
-    so that you can start your journey in writing.</h1>;
+    <div className="create-form">
+      <div className="nav-buttons">
+        <animated.button
+          onClick={createClicked}
+          id="loginBtn"
+          style={createBtnProps}
+        >
+          Create your own story
+        </animated.button>
+      </div>
+      <div className="form-group">
+        <animated.form action="" id="createform" style={createProps}>
+          <Form />
+        </animated.form>
+      </div>
     </div>
-  )
+  );
 }
+function Form() {
+  return (
+    <React.Fragment>
+      <label for="storyname">Story Name</label>
+      <input type="text" id="storyname" />
+      <label for="discription">Discription</label>
+      <input type="text" id="discription" />
+      <input type="submit" value="create" className="submit" />
+    </React.Fragment>
+  );
+}
+
+export default Create;
